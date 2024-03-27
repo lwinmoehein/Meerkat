1b2e634c0b15b6eb40ecd0187b61a006788f130c
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-03-27 18:06:47 +0400

    - add empty placeholder

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,7 @@ import '@radix-ui/themes/styles.css'
 import {deleteJob, getJobs} from "@/app/lib/actions";
 import {Badge, Box, Button, Card, DropdownMenu, Flex, Grid, Heading, Text} from "@radix-ui/themes";
 import AddJobDialog from "@/components/job/add-job-dialog";
-import {DotsVerticalIcon, Pencil1Icon, TrashIcon} from "@radix-ui/react-icons";
+import {DotsVerticalIcon, InfoCircledIcon, Pencil1Icon, TrashIcon} from "@radix-ui/react-icons";
 import JobItem from "@/components/job/job-item";
 
 
@@ -17,11 +17,23 @@ export default async function Page() {
                       </Heading>
                       <AddJobDialog/>
                   </Flex>
-                  <Grid columns={{initial:'1',md:'3'}} gap="5"  width="auto">
+                  {jobs&&jobs.length>0&& <Grid columns={{initial:'1',md:'3'}} gap="5"  width="auto">
                       {jobs&& jobs.map((job,index)=>
                           <JobItem key={job.id} job={job}/>
                       )}
-                  </Grid>
+                  </Grid>}
+
+                  {
+                      (jobs==null||jobs.length==0)&&
+                      <Card>
+                          <Flex minHeight={'200px'} justify={'center'} align={'center'}>
+                              <Flex align={'center'} gap={'1'}>
+                                  <InfoCircledIcon/>
+                                  <Text>No web pages were created yet.</Text>
+                              </Flex>
+                          </Flex>
+                      </Card>
+                  }
               </Box>
   )
 }
